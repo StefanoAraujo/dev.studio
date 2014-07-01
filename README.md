b369c820c60d0760414d98c74029cf28188546cf
dariel <darielnoel@gmail.com>
2014-06-30 22:13:42 -0430

    scalfolding

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
-dev.studio
-==========
+AniJS Studio
+============
 
-AniJS Studio Landing Page Develop Enviroment
+Add animations and interactions to any web easily.
+
+#Overview
+
+AniJS Studio es una extension de chrome, que le permitirá crear, probar y añadir prototipos de animaciones a cualquier pagina o aplicación web fácilmente.
+
+Gracias a su intuitiva interfaz de usuario, usted podrá con solo unos pocos click agregar las animaciones, sin tener que lidear con el código javascript o css.
+
+Usted podrá integrar las animaciones definidas con su entorno de producción con solo copiar y pegar.
+You can easily integrate it with your existing code. 
+
+
+It supports all modern desktop and mobile browsers. Work well in iPad, iPhone, Android and the modern browsers. ;)
+
+Está basado en la biblioteca de código abierto AniJS la cúal es Easy to use | Compact - around 7.7kb after gzipping | There is no need for third party libraries.
+
+#Examples
+
+See all the examples (here)[https://www.youtube.com/playlist?list=PLUVvd_W7urmg5iEmCATQS4ekARgyshUZa]
+
+If you are encorauge summit your own video to this, and share with others.
+
+#Learn 
+
+##The AniJS Sintax.
+
+Para agregar las animaciones se usa la sintaxis de AniJS.
+
+if (some event), on (some element), do(some animation), to(some target)
+
+Gif animado mostrando como se seleccionan las cosas.
+
+###Tutorial
+	Sera en la proxima version
+
+##Save and Real Code Integration.
+Para integrar las animaciones adicionadas con el codigo existente, usted debe completar 3 simples pasos.
+
+-1 Blal
+
+-2 bla bla
+
+-3 bla bla
+
+Gif animado de los pasos.
+
+###Tutorial
+	Sera en la proxima version
+
+
+##Importing data.
+	Usted puede importar el codigo generado previamente.
+
+#AniJS
+
+AniJS is a Declarative handling library for CSS animations, The main objective is to provide an eloquent, easy to translate, and quick to develop environment. you can find documentation and examples here.
+
+
+
+
---
+++ script/app.js
@@ -0,0 +1,86 @@
+YUI().use('node-base', 'node-event-delegate', 'autocomplete', 'autocomplete-highlighters','autocomplete-filters', 'transition', function(Y) {
+    
+
+    //Setting default class adding when anim
+    AniJS.setClassNamesWhenAnim('animated');
+
+    //Obtain AniJS default helper
+    var AniJSDefaultHelper = AniJS.getHelper();
+
+    //Put this function into the helper
+    AniJSDefaultHelper.validateCommandExecution = function(e, animationContext){
+        var event = Y.one('#demoif').get('value'),
+            eventTarget = Y.one('#demoon').get('value') || '#demotryit',
+            behaviorTarget = Y.one('#demoto').get('value') || '#demotryit',
+            behavior = Y.one('#demodo').get('value'),
+            successMessage = 'Animation setting up good!!! </br>' +
+                            'Please ' + event + ' on ' + eventTarget;
+
+        Y.one('#demo-message').set('innerHTML', successMessage);
+
+        //Clear the trigered event in every launch
+        AniJS.purge(eventTarget);
+
+        //Creating an animation in non declarative way
+        AniJS.createAnimation([{
+            event: event,
+            eventTarget: eventTarget,
+            behaviorTarget: behaviorTarget,
+            behavior: behavior,
+            after: 'removeAnim'
+        }]);
+
+        //Running show info wall animation
+        animationContext.run();
+        
+    };
+
+
+    
+
+    //Autocomplete
+    var autocompleSources = APP_CONFIG.autoCompleteSources,
+        autocompleteConfig = [
+            {rootNode:'#demoif', source: autocompleSources.if},
+            {rootNode:'#demoon', source: autocompleSources.on},
+            {rootNode:'#demoto', source: autocompleSources.on},
+            {rootNode:'#demodo', source: autocompleSources.do}
+        ];
+
+    Y.each(autocompleteConfig, function(item){
+        Y.one(item.rootNode).plug(Y.Plugin.AutoComplete, {
+            resultHighlighter: 'phraseMatch',
+            resultFilters: 'phraseMatch',
+            activateFirstItem: true,
+            source: item.source
+        });
+    });
+
+    // This just makes sure that the href="#" attached to the <a> elements
+    // don't scroll you back up the page.
+    Y.one('body').delegate('click', function(e) {
+        e.preventDefault();
+    }, 'a[href="#"]');
+
+    //All non internal links open in a new tab
+    var links = Y.all('a');
+    links.each(function(a){
+        if(a.getAttribute('href')[0] !== '#')
+            a.setAttribute('target', '_blank');
+    });
+
+    // //Nav responsive behavior
+    // Y.one('.collapse-toggle').on('click', function(e){
+    //     e.preventDefault();
+    //     Y.one('.collapse').toggleClass('in');
+    // });
+
+    // //The menu items are smart behavior
+    // Y.one('body').delegate('click', function(e) {
+    //     if (!e.currentTarget.hasClass('pure-menu-selected')) {
+    //         Y.one('.header li.pure-menu-selected').removeClass('pure-menu-selected');
+    //         e.currentTarget.get('parentNode').addClass('pure-menu-selected');
+    //     }
+    // }, '.header li a');
+
+});
